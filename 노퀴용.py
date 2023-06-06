bf24c8c4e7bd294ceab4a366ea55c6becc8c3f25
world-DESIGNER <97209446+world-DESIGNER@users.noreply.github.com>
2023-06-06 11:31:50 +0900

    Add files via upload

--- 노퀴용.py
+++ 노퀴용.py
@@ -1,8 +1,24 @@
 import os
+import random
+import string
 import youtube_dl
 from pydub import AudioSegment
 
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+def process_ogg_files(directory, id_mapping):
+    for filename in os.listdir(directory):
+        if filename.endswith('.ogg'):
+            base, ext = os.path.splitext(filename)
+            if base in id_mapping:
+                os.rename(os.path.join(directory, filename), os.path.join(directory, id_mapping[base] + ext))
+
 def download_and_convert(playlist_url, duration, bitrate, trim_audio):
+    id_mapping = {}
+
     ydl_opts = {
         'format': 'bestaudio',
         'outtmpl': '%(id)s.%(ext)s',
@@ -21,6 +37,8 @@ def download_and_convert(playlist_url, duration, bitrate, trim_audio):
 
     for video_info in video_infos:
         video_id = video_info['id']
+        random_str = get_random_string(4)
+        id_mapping[video_id] = random_str
 
         if not os.path.exists(f"{video_id}.wav"):
             print(f"{video_id} 다운로드 실패")
@@ -31,17 +49,17 @@ def download_and_convert(playlist_url, duration, bitrate, trim_audio):
             try:
                 audio = AudioSegment.from_wav(f"{video_id}.wav")
                 audio = audio[:duration*1000]  # Get the first 'duration' seconds
-                audio.export(f"{video_id}_trimmed.wav", format="wav")
+                audio.export(f"{random_str}_trimmed.wav", format="wav")
             except Exception as e:
                 print(f"{video_id} 자르기 실패: {e}")
                 continue
 
-            input_file = f"{video_id}_trimmed.wav"
+            input_file = f"{random_str}_trimmed.wav"
         else:
             input_file = f"{video_id}.wav"
 
         # Convert to ogg using ffmpeg
-        conversion_result = os.system(f"ffmpeg -i {input_file} -b:a {bitrate} {video_id}.ogg")
+        conversion_result = os.system(f"ffmpeg -i {input_file} -b:a {bitrate} {random_str}.ogg")
 
         if conversion_result != 0:
             print(f"{video_id} 변환 실패")
@@ -49,12 +67,15 @@ def download_and_convert(playlist_url, duration, bitrate, trim_audio):
         # Delete the original and trimmed wav files
         os.remove(f"{video_id}.wav")
         if trim_audio:
-            os.remove(f"{video_id}_trimmed.wav")
+            os.remove(f"{random_str}_trimmed.wav")
 
     # Write video ids and titles to a text file
     with open('노래목록.txt', 'w', encoding='utf-8') as f:
         for video_info in video_infos:
-            f.write(f"{video_info['id']}|{video_info['title']}|\n")
+            f.write(f"{id_mapping[video_info['id']]}|{video_info['title']}|\n")
+
+    # process the ogg files
+    process_ogg_files(".", id_mapping)
 
 # Get user input for bitrate and whether to trim the audio
 bitrate = input("추출된 오디오의 비트레이트를 비트 단위로 입력하십시오 (예: 64k 또는 128k): ")
